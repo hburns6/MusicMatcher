d2fa7c3683a4e27129b1aeb21ead93c34b1ec561
hburns6 <98904471+hburns6@users.noreply.github.com>
2023-10-25 13:19:30 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,103 @@
+
+
+
+const express = require('express');
+
+const app = express();
+
+app.set("views", "./views");
+app.set("view engine", "pug");
+
+app.use(express.static("public"));
+
+const redirect_uri ="http://localhost:3000/callback";
+const client_id = "33c3a45c1db24e6aa74fe79b268eadfb";
+const client_secret = "21f632a9010a4c8782466d22eec429ef"
+
+app.get("/", function (req, res){
+    res.render("index");
+});
+
+app.get("/authorize", (req, res) => {
+   var auth_query_parameters = new URLSearchParams ({
+    response_type: "code",
+    client_id: client_id,
+    scope: "user-library-read",
+    redirect_uri: redirect_uri
+  }) 
+
+  res.redirect("https://accounts.spotify.com/authorize?" + auth_query_parameters.toString());
+
+});
+
+app.get("/callback", async(req, res) => {
+    const code = req.query.code;
+    
+    var body = new URLSearchParams ({
+        code: code,
+        redirect_uri: redirect_uri,
+        grant_type: "authorization_code"
+
+    })
+
+    const response = await fetch('https://accounts.spotify.com/api/token', {
+        method: "post",
+        body: body,
+        headers: {
+            "Content-type": "application/x-www-form-urlencoded",
+            "Authorization": "Basic " +  
+             Buffer.from(client_id + ":" + client_secret).toString("base64"),
+        }
+    })
+
+    const data = await response.json();
+    global.access_token = data.access_token;
+
+    res.redirect("/dashboard")
+ });
+async function getData(endpoint) {
+    const response = await fetch("https://api.spotify.com/v1" + endpoint, {
+        method: "get",
+        headers: {
+            Authorization: "Bearer " + global.access_token
+        }
+    });
+    const data = await response.json();
+    return data;
+}
+
+ app.get("/dashboard", async(req, res) => { 
+  
+    const userInfo = await getData('/me');
+    const tracks = await getData('/me/tracks?limit=10');
+
+    res.render("dashboard", {user: userInfo, tracks: tracks.items});
+
+ });
+
+ app.get("/recommendations", async(req, res) => { 
+    const artist_id = req.query.artist;
+    const track_id = req.query.track;
+    const params = new URLSearchParams({
+        seed_artist: artist_id,
+        seed_tracks: track_id,
+    })
+    
+    const data = await getData('/recommendations?' + params);
+    
+    res.render("recommendation", {tracks: data.tracks})
+
+
+
+ })
+
+
+
+
+
+
+
+let listener = app.listen(3000, function () {
+console.log("your app is listening on http://localhost:" + 
+listener.address().port);
+});
